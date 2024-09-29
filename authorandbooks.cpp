983bc2519bd7ec8cb412ca97d06758e289b538cb
Bhavya <bgprajapati575@gmail.com>
2024-09-29 21:20:02 +0530

    init

---
+++ authorandbooks.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> arrangeAuthors(vector<vector<string>> &s)
+{
+    vector<string> ans;
+    for (int i = 0; i < s.size(); i++)
+    {
+        for (int j = 0; j < s[i].size(); j++)
+        {
+            if (j == 0)
+            {
+                ans.push_back(to_string(i + 1) + ". " + s[i][j]);
+            }
+            else{
+                string bookname = string(1, 'A' + (j - 1)) + ". " + s[i][j];
+            ans.push_back(bookname);
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<vector<string>> s = {
+        {"ChetanBhagat", "TwoStates", "Revolution", "HalfGirlfriend", "OneIndianGirl"},
+        {"JKRowling", "HarryPotter", "FantasticBeasts"},
+        {"JeffreyArcher", "OldLove", "FalseImpression"}};
+
+    vector<string> ans = arrangeAuthors(s);
+    for (auto a : ans)
+    {
+        cout << a << endl;
+    }
+    return 0;
+}
