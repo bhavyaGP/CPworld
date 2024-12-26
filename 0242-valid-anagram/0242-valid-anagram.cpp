79265bc490a1017af79a9a4e46cc6f6567451fdf
Bhavya Prajapati <126806988+bhavyaGP@users.noreply.github.com>
2024-12-26 19:09:42 +0530

    Time: 1 ms (68.90%), Space: 10 MB (11.58%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    if(s.size() != t.size()) {
+        return false;
+    }
+    unordered_map<char, int> mp;
+    for(auto x : s) {
+        mp[x]++;
+    }
+    for(auto x : t) {
+        mp[x]--;
+        if(mp[x] < 0) {
+            return false;
+        }
+    }
+    for(auto x : mp) {
+        if(x.second != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+};
