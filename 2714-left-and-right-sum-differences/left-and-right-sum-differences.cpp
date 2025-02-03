072e331e11590660ca0891348a45daf061e3ac0d
Bhavya Prajapati <126806988+bhavyaGP@users.noreply.github.com>
2025-02-03 18:44:55 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (61.64%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
+    //space complexity O(n);
     vector<int> leftRightDifference(vector<int>& nums) {
-        vector<int> left(nums.size());
-        vector<int> right(nums.size());
-
-        left[0] = 0;
-        for (int i = 1; i < nums.size(); i++) {
-            left[i] = left[i - 1] + nums[i - 1];
+        int rightsum = 0;
+        for (auto x : nums) {
+            rightsum += x;
         }
-        right[nums.size() - 1] = 0;
-        for (int i = nums.size() - 2; i >= 0; i--) {
-            right[i] = right[i + 1] + nums[i + 1];
-        }
+        int leftsum = 0;
         vector<int> ans(nums.size());
-        for (int i = 0; i < nums.size(); i++) {
-            ans[i] = abs(left[i] - right[i]);
+        for (int i = 0; i < ans.size(); i++) {
+            rightsum -= nums[i];
+            ans[i] = abs(leftsum - rightsum);
+            leftsum += nums[i];
         }
         return ans;
     }
