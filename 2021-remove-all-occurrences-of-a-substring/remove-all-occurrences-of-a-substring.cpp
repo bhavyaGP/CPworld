963baa43d0ad06e7562c8ea8d8c5c2107e17e071
Bhavya Prajapati <126806988+bhavyaGP@users.noreply.github.com>
2025-02-11 18:05:53 +0530

    Time: 3 ms (32.43%) | Memory: 12.3 MB (7.07%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-    bool check(stack<char>& st, string& part, int n) {
-        stack<char> tempSt = st;
-
-        for(int idx = n-1; idx >= 0; idx--) {
-            if(tempSt.top() != part[idx]) {
-                return false;
-            }
-
-            tempSt.pop();
-        }
-
-        return true;
-    }
     string removeOccurrences(string s, string part) {
-        stack<char> st;
-        for(int i=0;i<s.length();i++){
-            st.push(s[i]);
-            if(st.size()>=part.length() && check(st,part,part.length())==true){
-                for(int k=0;k<part.length();k++){
-                    st.pop();
-                }
+        string result = "";
+        int n = part.length();
+
+        for(auto ch : s) {
+            result.push_back(ch);
+
+            if(result.length() >= n && result.substr(result.length() - n) == part) {
+                result.erase(result.length() - n);
             }
         }
-        string ans="";
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+
+        return result;
     }
 };
