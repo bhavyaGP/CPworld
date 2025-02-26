67e866a9d753d8eb98d4cf484f96aa30d170ea3c
Bhavya Prajapati <126806988+bhavyaGP@users.noreply.github.com>
2025-02-26 11:58:33 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (78.26%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        int currentmaxsum=nums[0],maxsum=nums[0];
-        for(int i=1;i<nums.size();i++){
-            currentmaxsum=max(nums[i],currentmaxsum+nums[i]);
-            maxsum=max(currentmaxsum,maxsum);
+        int currentmaxsum = nums[0], maxsum = nums[0], currentminsum = nums[0],
+            minsum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            currentmaxsum = max(nums[i], currentmaxsum + nums[i]);
+            currentminsum = min(nums[i], currentminsum + nums[i]);
+            maxsum = max(currentmaxsum, maxsum);
+            minsum = min(currentminsum, minsum);
         }
-        int currentminsum=nums[0],minsum=nums[0];
-        for(int i=1;i<nums.size();i++){
-            currentminsum=min(nums[i],currentminsum+nums[i]);
-            minsum=min(currentminsum,minsum);
-        }
-        return max(abs(minsum),abs(maxsum));
+
+        return max(abs(minsum), abs(maxsum));
     }
 };
