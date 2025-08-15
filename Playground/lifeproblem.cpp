b133047340d59f05a4e88da901672072ef8000e5
Bhavya <bgprajapati575@gmail.com>
2025-08-15 18:39:12 +0530

    clear

---
+++ Playground/lifeproblem.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int printlift(int reqfloor){
+    
+}
+int main(){
+
+
+    vector<int> request={3, 8, 2, 6};
+    int floors=9;
+    for(auto x:request){
+
+    }
+
+
+}
