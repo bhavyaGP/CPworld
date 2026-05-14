711ddc71edefadb39027f3c80b2e54fc0e0429ea
Bhavya Prajapati <126806988+bhavyaGP@users.noreply.github.com>
2026-05-14 21:48:46 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (95.68%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //brute force approach 
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                return i; 
+        int l=0,h=nums.size()-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            //left half sorted hai kya?
+            if(nums[l]<=nums[mid]){
+                if(target>=nums[l] && target<nums[mid]){ //window ko aur chota karo 
+                    h=mid-1;
+                }else{
+                    l=mid+1; // nahi mila matlab woh pakka right side me hai  // 4 5 6 7 0 1 2 : target=1 
+                }
+            }else{ 
+                if(target>nums[mid]&& target<=nums[h]){ //window ko chota karo 
+                    l=mid+1;
+                }else{
+                    h=mid-1; //woh left side mai hai 
+                }
             }
         }
         return -1;
