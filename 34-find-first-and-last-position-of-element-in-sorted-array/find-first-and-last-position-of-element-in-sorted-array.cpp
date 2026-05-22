7c0626f013cbe0239598602c77166689645428ca
Bhavya Prajapati <126806988+bhavyaGP@users.noreply.github.com>
2026-05-22 20:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (52.85%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int cnt=0;
-        int left=-1,right=-1;
-        for(int i=0;i<nums.size();i++){
-            if(target==nums[i]){
-                if(cnt==0){
-                    left=i;
-                }
-                right=i;
-                cnt++;
+    int rightmost(vector<int>& nums, int target){
+        int left=0,right=nums.size()-1;
+        int rightMost=-1; //if don't get any idx then -1 not 0
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                rightMost=mid; //ho sakte hai mera answer 
+                left=mid+1; //right me aur target element hai kya?
+            }else if(target>nums[mid]){
+                left=mid+1;
+            }else{
+                right=mid-1;
             }
         }
-        return {left,right};
+        return rightMost;
+    }
+    int leftmost(vector<int>& nums, int target){
+        int left=0,right=nums.size()-1;
+        int leftMost=-1;//if don't get any idx then -1 not 0
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                leftMost=mid; //ho sakte hai mera answer 
+                right=mid-1; //left me aur target element hai kya?
+            }else if(target>nums[mid]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return leftMost;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {leftmost(nums,target),rightmost(nums,target)};
     }
 };
